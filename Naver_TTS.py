f0d7f349f60de467a85b4871a648ee3f473a9784
Youngjun Kim <hipizza0527@gmail.com>
2023-10-02 17:27:01 +0900

    feat: Class 최적화

--- Naver_TTS.py
+++ Naver_TTS.py
@@ -9,25 +9,30 @@ import io
 load_dotenv() # 실제로는 main 함수에서 실행
 
 class NaverTTS:
-    def __init__(self):
+    HEADERS = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    URL = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
+
+    def __init__(self, speaker="nwoof"):
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
+        self.HEADERS["X-NCP-APIGW-API-KEY-ID"] = self.client_id
+        self.HEADERS["X-NCP-APIGW-API-KEY"] = self.client_secret
+        self.speaker = speaker
         self.p = pyaudio.PyAudio()
 
-
-    def generate_and_play_speech(self, text, speaker="nwoof", volume=0, speed=0, pitch=0, audio_format="mp3"):
-        print("text: ", text)
-        source_text = requests.utils.quote(text)
-        data = f"speaker={speaker}&volume={volume}&speed={speed}&pitch={pitch}&format={audio_format}&text=" + source_text
-        url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
-        
-        headers = {
-            "X-NCP-APIGW-API-KEY-ID": self.client_id,
-            "X-NCP-APIGW-API-KEY": self.client_secret,
-            "Content-Type": "application/x-www-form-urlencoded"
+    def generate_audio_and_play(self, text, volume=0, speed=0, pitch=0, audio_format="mp3"):
+        print("streaming text: ", text)
+        data = {
+            "speaker": self.speaker,
+            "volume": volume,
+            "speed": speed,
+            "pitch": pitch,
+            "format": audio_format,
+            "text": text
         }
-        
-        response = requests.post(url, headers=headers, data=data.encode('utf-8'))
+        response = requests.post(self.URL, headers=self.HEADERS, data=data)
 
         if response.status_code == 200:
             print("TTS 스트리밍 재생")
@@ -36,21 +41,25 @@ class NaverTTS:
             print(f"Error Code: {response.status_code}")
 
     def stream_audio(self, audio_data):
-        audio = AudioSegment.from_mp3(io.BytesIO(audio_data))
-        audio = audio.set_channels(1).set_frame_rate(22050)
-        audio_data = audio.raw_data
+        try:
+            audio = AudioSegment.from_mp3(io.BytesIO(audio_data))
+            audio = audio.set_channels(1).set_frame_rate(22050)
+            audio_data = audio.raw_data
 
-        stream = self.p.open(format=self.p.get_format_from_width(audio.sample_width),
-                             channels=1,
-                             rate=22050,
-                             output=True)
-        stream.write(audio_data)
-
-        stream.stop_stream()
-        stream.close()
+            stream = self.p.open(format=self.p.get_format_from_width(audio.sample_width),
+                                 channels=1,
+                                 rate=22050,
+                                 output=True)
+            stream.write(audio_data)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            if stream:
+                stream.stop_stream()
+                stream.close()
 
 if __name__ == "__main__":
     tts = NaverTTS()
     text = "안녕하세요 저는 이루멍이에요"
-    tts.generate_and_play_speech(text)
+    tts.generate_audio_and_play(text)
 
