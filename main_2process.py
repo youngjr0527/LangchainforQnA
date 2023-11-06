f6cfde272918c5f7b977305d1064d538d9b21c93
Youngjun Kim <hipizza0527@gmail.com>
2023-11-06 20:21:40 +0900

    feat: 프로세스 구분 초안

--- main_2process.py
+++ main_2process.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-from threading import Thread
+from multiprocessing import Process, Event
 from naver_TTS import TTS_Agent
 from google_STT import STT_Agent
 from Integrate_LangChain import CampusGuideBot
@@ -16,12 +16,11 @@ parser.add_argument('--update', type=bool, default=False, help='Update LLM with 
 args = parser.parse_args()
 update_LLM = False
 
-# main 함수
-if __name__ == "__main__":
+def process_A(event):
     stt = STT_Agent()
     bot = CampusGuideBot()
     tts = TTS_Agent()
-    
+
     if args.update or update_LLM:
         logging.info("Updating LLM...")
         menu_scraper = UOSMenuScraper(save_dir='UOS_DB')
@@ -30,9 +29,10 @@ if __name__ == "__main__":
         notice_scraper.run()
         bot.ingest_documents()
         logging.info("LLM updated!")
-        
+
     stt.setup_mic()
-    while True:
+
+    while not event.is_set():  # Event가 set 상태가 아닐 때만 실행
         if stt.listen_for_trigger():
             logging.info("호출 명령어가 인식되었습니다.")
             tts.generate_audio_and_play("네, 무엇을 도와드릴까요?")
@@ -77,3 +77,35 @@ if __name__ == "__main__":
                 continue
         else:
             continue
+
+        # TODO: 프로세스 B를 시작해야 할 조건(GPS 신호가 들억오면)이 충족되면 event.set() 호출 -> True
+
+
+# 프로세스 B의 함수
+def process_B(event):
+    # 프로세스 B의 로직
+    while event.is_set():  # Event가 set 상태일 때만 실행
+        # 프로세스 B의 작업 수행
+        # ...
+        # 프로세스 A를 재개해야 할 조건이 충족되면 event.clear() 호출
+        pass
+
+# main 함수
+if __name__ == "__main__":
+    event = Event()  
+    # event가 발생하면(GPS 신호가 들어와 멘트를 출력해야 할 때) set() 호출
+    # event.set() 호출하여 event 상태가 True가 되면 Process B가 실행됨
+    # event.clear() 호출하여 event 상태가 False가 되면 Process A가 실행됨
+    
+    # 프로세스 A와 B 시작
+    pa = Process(target=process_A, args=(event,))
+    pb = Process(target=process_B, args=(event,))
+    event.clear()
+    pa.start()
+    pb.start()
+
+    # 프로세스들이 종료될 때까지 기다림
+    pa.join()
+    pb.join()
+        
+
